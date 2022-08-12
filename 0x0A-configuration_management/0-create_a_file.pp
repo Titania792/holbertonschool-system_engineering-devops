9aa6ccdbdc40f56475a88ee18d6c29f47959b812
Titania792 <darkwarrior792@gmail.com>
2022-08-12 17:00:11 -0300

    trying to fix task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
-file {
-    '/tmp/school':
-    owner   => www-data,
-    group   => www-data,
+file {'/tmp/school':
+    owner   => 'www-data',
+    group   => 'www-data',
     mode    => '0744',
     content => 'I love Puppet'
 }
