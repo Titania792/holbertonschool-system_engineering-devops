5740454cbf27d42cd2123b2fc7fd5ed4677e2403
Tatiana Fernández <darkwarrior792@gmail.com>
2022-09-06 18:05:08 -0300

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" extend your Python script to export data in the CSV format """
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    u_id = argv[1]
+    api_url = "https://jsonplaceholder.typicode.com/users/{}".format(u_id)
+    api_url2 = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        .format(u_id)
+    response = requests.get(api_url).json()
+    EMPLOYEE_NAME = response.get('name').split(' ')
+    response = requests.get(api_url2).json()
+    for info in response:
+        TASK_COMPLETED_STATUS = info.get("completed")
+        TASK_TITLE = info.get("title")
+        print('"{}","{}","{}","{}"'.format(
+            u_id, EMPLOYEE_NAME[0], TASK_COMPLETED_STATUS, TASK_TITLE))
