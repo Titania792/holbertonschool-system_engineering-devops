caff7bc54173126904b5789d5b185091d16162d7
Tatiana Fernández <darkwarrior792@gmail.com>
2022-09-21 18:44:41 -0300

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Function that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit """
+import requests
+
+
+def top_ten(subreddit):
+    """ If not a valid subreddit, print None """
+    try:
+        api_url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(
+            subreddit)
+        response = requests.get(
+            api_url, headers={"User-Agent": "Mozilla/5.0"})
+        response = response.json()['data']['children']
+        for title in response:
+            print(title['data']['title'])
+    except Exception:
+        return 0
